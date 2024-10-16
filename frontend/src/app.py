7d9a207ac6152979fd4763ce0b2157ff128b8675
AlanSilvaaa <asilvar4@correo.uss.cl>
2024-10-16 09:54:06 -0300

    tailwind

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -4,37 +4,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 app.secret_key = 'magickey'
 
-# Set up session for SQLAlchemy (Commented out for now)
-# Session = sessionmaker(bind=engine)
-# session = Session()
-
-# Route for login page
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        username = request.form.get('username')
-        password = request.form.get('password')
-        
-        # Logic to validate user
-        # For example, checking if user and password are correct.
-        
-        # REVISE: https://webdamn.com/login-and-registration-with-python-flask-mysql/cl
-
-        # If validation is correct, redirects to homepage (index.html)
-        if username == 'user' and password == 'pass':  # Validación simple de ejemplo
-            return redirect(url_for('menu'))
-        else:
-            return render_template('login.html', error="Usuario o contraseña incorrectos")
-    return render_template('login.html')
-
-# Route for menu page (homepage)
-@app.route('/menu')
-def menu():
-    return render_template('index.html')
-
-@app.route('/test')
+@app.route('/')
 def test():
-    return render_template('test.html')
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
