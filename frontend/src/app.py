385df04efd1d32c7ff0705daff47cefdc04ceded
AlanSilvaaa <asilvar4@correo.uss.cl>
2024-11-04 15:51:30 -0300

    icons and shops

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -4,9 +4,9 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 app.secret_key = 'magickey'
 
-@app.route('/')
+@app.route('/home')
 def test():
-    return render_template('index.html')
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
