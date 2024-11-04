8ceb4db7e3f593200349a73234ca62d0fa77fa48
AlanSilvaaa <asilvar4@correo.uss.cl>
2024-11-04 16:47:35 -0300

    add initial user page

--- frontend/src/app.py
+++ frontend/src/app.py
@@ -4,9 +4,16 @@ from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 app.secret_key = 'magickey'
 
-@app.route('/home')
-def test():
+@app.route('/')
+def index():
     return render_template('home.html')
 
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route("/user")
+def user():
+    return render_template('user.html')
 if __name__ == '__main__':
     app.run(debug=True)
